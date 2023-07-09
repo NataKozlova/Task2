24225712df4f625fd1fa07fa69e622311b5071d0
natakozlova <natabalabanova555@yandex.ru>
2023-07-09 13:19:44 -0700

    add tests

--- src/test/java/TestInput.java
+++ src/test/java/TestInput.java
@@ -6,6 +6,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -15,6 +16,12 @@ import static io.restassured.RestAssured.given;
 
 
 public class TestInput {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+
     private static RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
@@ -23,61 +30,49 @@ public class TestInput {
             .log(LogDetail.ALL)
             .build();
 
-    @BeforeAll
-    static void setUpAll() {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new RegistrationDto("ivan", "password", "blocked")) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
-    }
-
     @Test
     void shouldSomethingCheck() {
-        Configuration.headless = true;
-        open("http://localhost:9999");
-        $(By.cssSelector("[data-test-id='login'] input")).setValue("vasya");
-        $(By.cssSelector("[data-test-id='password'] input")).setValue("password");
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        $(By.cssSelector("[data-test-id='login'] input")).setValue(registeredUser.getLogin());
+        $(By.cssSelector("[data-test-id='password'] input")).setValue(registeredUser.getPassword());
         $(By.cssSelector("[data-test-id='action-login']")).click();
         $(Selectors.byText("Личный кабинет")).should(Condition.appear);
     }
 
     @Test
-    void shouldCheckBlockedUser() {
-        Configuration.headless = true;
+    void shouldCheckNotRegistrationUser() {
         open("http://localhost:9999");
-        $(By.cssSelector("[data-test-id='login'] input")).setValue("ivan");
-        $(By.cssSelector("[data-test-id='password'] input")).setValue("password");
+        var notRegisteredUser = DataGenerator.Registration.getUser("active");
+        $(By.cssSelector("[data-test-id='login'] input")).setValue(notRegisteredUser.getLogin());
+        $(By.cssSelector("[data-test-id='password'] input")).setValue(notRegisteredUser.getPassword());
+        $(By.cssSelector("[data-test-id='action-login']")).click();
+        $(By.cssSelector("[data-test-id='error-notification'] .notification__content")).should(Condition.text("Ошибка! Неверно указан логин или пароль")).shouldBe(Condition.visible);
+    }
+    @Test
+    void shouldCheckBlockedUser() {
+        var blockedUser = DataGenerator.Registration.getRegisteredUser("blocked");
+        $(By.cssSelector("[data-test-id='login'] input")).setValue(blockedUser.getLogin());
+        $(By.cssSelector("[data-test-id='password'] input")).setValue(blockedUser.getPassword());
         $(By.cssSelector("[data-test-id='action-login']")).click();
         $(Selectors.byText("Пользователь заблокирован")).should(Condition.appear);
     }
 
     @Test
     void shouldCheckLoginUser() {
-        Configuration.headless = true;
-        open("http://localhost:9999");
-        $(By.cssSelector("[data-test-id='login'] input")).setValue("mark");
-        $(By.cssSelector("[data-test-id='password'] input")).setValue("password");
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        var wrongLogin = DataGenerator.getRandomLogin();
+        $(By.cssSelector("[data-test-id='login'] input")).setValue(wrongLogin);
+        $(By.cssSelector("[data-test-id='password'] input")).setValue(registeredUser.getPassword());
         $(By.cssSelector("[data-test-id='action-login']")).click();
         $(Selectors.byText("Неверно указан логин или пароль")).should(Condition.appear);
     }
 
     @Test
     void shouldCheckPasswordUser() {
-        Configuration.headless = true;
-        open("http://localhost:9999");
-        $(By.cssSelector("[data-test-id='login'] input")).setValue("vasya");
-        $(By.cssSelector("[data-test-id='password'] input")).setValue("01010101");
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        var wrongPassword = DataGenerator.getRandomPassword();
+        $(By.cssSelector("[data-test-id='login'] input")).setValue(registeredUser.getLogin());
+        $(By.cssSelector("[data-test-id='password'] input")).setValue(wrongPassword);
         $(By.cssSelector("[data-test-id='action-login']")).click();
         $(Selectors.byText("Неверно указан логин или пароль")).should(Condition.appear);
     }
